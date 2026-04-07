18b9454b5112b6aa1967897d80d2ab829d382754
Prerna Raj <prernaraj2801@gmail.com>
2026-04-07 18:44:54 +0530

    Added School website (basic) project

---
+++ School Website/script.js
@@ -0,0 +1,14 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);  //.substring(1) removes the # symbol, leaving just "about".
+    const targetSection = document.getElementById(targetId);
+    window.scrollTo({
+      top: targetSection.offsetTop - 60, // adjust for header height
+      behavior: 'smooth'
+    });
+  });
+});
+
+//class ke . use krte h; # for id 
